739ceee682e1dffdb19ddd7d83c4cf7d0a95abcc
Yustikk <anaiustina99@gmail.com>
2025-05-19 18:49:23 +0300

    Created page layout and adde basic operation functions (+,-,/,etc)

---
+++ script.js
@@ -0,0 +1,26 @@
+let a=3;
+let b=5;
+
+//operation functions!------------------------------------------------------------------------
+function add(a,b)
+{
+    return a+b;
+}
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+function divide(a,b)
+{
+    return a/b;
+}
+
+function substract(a,b)
+{
+    return a-b;
+}
+
+//before showing a number add .tofixed(2)
+
