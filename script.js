86fa5508a123bb31903b1ca2091a60af18ae6634
Yustikk <anaiustina99@gmail.com>
2025-05-19 20:13:55 +0300

    Added event listeners to buttons

--- script.js
+++ script.js
@@ -22,5 +22,24 @@ function substract(a,b)
     return a-b;
 }
 
+function precent(a)
+{
+    return a/100;
+}
+
+function changesign(a)
+{
+    return a*(-1);
+}
+
 //before showing a number add .tofixed(2)
 
+//Making the buttons functional----------------------------------------------------------------
+
+let buttons=document.querySelectorAll("button");
+console.log(buttons);
+buttons.forEach( (button) => button.addEventListener("click", function(e){
+    console.log(e.target.innerText+" has been pressed!")
+    let item=e.target.innerText;
+    
+}))
