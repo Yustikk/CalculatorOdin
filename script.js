c063139999bd366e2c767213d188f9c983df316d
Yustikk <anaiustina99@gmail.com>
2025-05-20 18:52:58 +0300

    solved multiple dots in one number
    solved not reseting after equal is pressed while also being able to use the result for a new operation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let a=3;
-let b=5;
+
 
 //operation functions!------------------------------------------------------------------------
 function add(a,b)
@@ -32,14 +31,108 @@ function changesign(a)
     return a*(-1);
 }
 
+function operate(a,operation,b)
+{
+    if(operation!="")
+    {
+            result=true;
+                    
+           switch(operation)
+            {
+                case "+":
+                    number1=String(add(a,b));
+                    break;
+                case "-":
+                    number1=String(substract(a,b));
+                    break;
+                case "*":
+                    number1=String(multiply(a,b));
+                    break;
+                case "/":
+                    number1=String(divide(a,b));
+                    break;
+            }
+                    
+    }
+}
+
 //before showing a number add .tofixed(2)
 
+//making the display!
+
+let display=document.getElementById("display");
+
 //Making the buttons functional----------------------------------------------------------------
+let number1='';
+let number2='';
+let operand='';
+let result=false;
+
 
 let buttons=document.querySelectorAll("button");
 console.log(buttons);
 buttons.forEach( (button) => button.addEventListener("click", function(e){
-    console.log(e.target.innerText+" has been pressed!")
+    
     let item=e.target.innerText;
     
+    if((item <=9 && item>=0)|| item==".")
+    {   
+        if(!(item=="." && number2.indexOf(".")!=-1))
+        {
+                if(result)
+        {
+            number1='';
+                number2='';
+        }
+        result=false;
+        number2=number2+item;
+        display.innerText=number2.slice(0,7);
+        }
+        
+        
+
+    }
+    else
+    {
+        result=false;
+        if(number1=='')
+            number1=number2;
+        switch(item)
+        {
+
+            case "AC":
+                display.innerHTML="";
+                number1='';
+                number2='';
+                operand="";
+                break;
+            case "+/-":
+                number1=changesign(Number(number1));
+                break;
+            case "%":
+                number1=precent(Number(number1));
+                break;
+            case "=":
+                a=Number(number1);
+                b=Number(number2);
+                operate(a,operand,b);
+                break;
+            
+            default:
+                operand=item;
+                number2="";
+                break;
+                
+        }
+        display.innerText=number1.slice(0,7);
+        
+        
+    }
+            console.log(number1+" "+operand+" " +number2+"\t"+result);
+
 }))
+
+function verifyNumberLenght()
+{
+    return (number2.length<7);
+}
